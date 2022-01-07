5e0e0d9a7df4e05bc11fc255674eb17383fba36f
ton-tonton <phantom.ness@windowslive.com>
2022-01-07 09:27:40 +0700

    Add comments resource

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
 class Post < ApplicationRecord
+  has_many :comments, dependent: :destroy
+
   has_rich_text :body
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_06_161047) do
+ActiveRecord::Schema.define(version: 2022_01_07_011725) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,6 +53,15 @@ ActiveRecord::Schema.define(version: 2022_01_06_161047) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
+  create_table "comments", force: :cascade do |t|
+    t.string "name"
+    t.string "body"
+    t.bigint "post_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+  end
+
   create_table "posts", force: :cascade do |t|
     t.text "body"
     t.boolean "published"
@@ -62,4 +71,5 @@ ActiveRecord::Schema.define(version: 2022_01_06_161047) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "comments", "posts"
 end
